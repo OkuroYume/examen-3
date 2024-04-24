427e78ffb04ed1bcc7e8311b76490cd2648f2d94
OkuroYume <yuma.comazzi@studentfr.ch>
2024-04-24 11:49:13 +0200

    first version

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 var hiddenformBurger = document.querySelector(".hiddenform-burger");
 var hiddenformMenu = document.querySelector(".hiddenform-menu");
+var hiddenformArrow = document.querySelector(".hiddenform-arrow");
 var toggleMenu = function toggleMenu() {
   hiddenformMenu.classList.toggle("is-active");
+  hiddenformArrow.classList.toggle("rotated");
 };
 hiddenformBurger.addEventListener("click", toggleMenu);
